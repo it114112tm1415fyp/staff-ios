11216254ee21c3a2714c2c652cdbe944ceaf74db
VM tlsv <6y@Mac6y.local>
2014-12-10 15:09:07 +0800

    objective-C HTTP6y 1.0

--- Staff-IOS/HTTP6y.m
+++ Staff-IOS/HTTP6y.m
@@ -8,6 +8,19 @@
 
 #import "HTTP6y.h"
 
+/*Cache-Control → max-age=0, private, must-revalidate
+ Connection → Keep-Alive
+ Content-Length → 274
+ Content-Type → application/json; charset=utf-8
+ Date → Tue, 09 Dec 2014 08:11:38 GMT
+ Etag → "94379f78a6f23890017ff27255839665"
+ Server → WEBrick/1.3.1 (Ruby/2.1.3/2014-09-19)
+ X-Content-Type-Options → nosniff
+ X-Frame-Options → SAMEORIGIN
+ X-Request-Id → cd1e85e9-b40a-4e60-8290-2a8e68d19f82
+ X-Runtime → 0.208000
+ X-Xss-Protection → 1; mode=block*/
+
 static NSDictionary* cookie;
 static NSString* ServerUrl = @"http://it114112tm1415fyp1.redirectme.net:8000/";
 
@@ -25,31 +38,31 @@ static NSString* ServerUrl = @"http://it114112tm1415fyp1.redirectme.net:8000/";
     return [self request:postposition parameters:[NSDictionary new] arrayParameters:arrayParameters];
 }
 
-+ (NSDictionary*)request:(NSString*)postposition parameters:(NSMutableDictionary*)parameters arrayParameters:(NSDictionary*)arrayParameters {
++ (NSDictionary*)request:(NSString*)postposition parameters:(NSDictionary*)parameters arrayParameters:(NSDictionary*)arrayParameters {
     NSError* error = nil;
     NSString* formatedParameters = @"";
     if(parameters != nil && parameters.count != 0) {
         for(NSString* x in parameters) {
             formatedParameters = [formatedParameters stringByAppendingString:x];
-            NSLog(@"%@",x);
             formatedParameters = [formatedParameters stringByAppendingString:@"="];
-            NSLog(@"%@",(NSString*) [parameters objectForKey:x]);
             formatedParameters = [formatedParameters stringByAppendingString:(NSString*) [parameters objectForKey:x]];
-            NSLog(@"3");
             formatedParameters = [formatedParameters stringByAppendingString:@"&"];
-            NSLog(@"4");
         }
         formatedParameters = [formatedParameters substringToIndex:[formatedParameters length] - 1];
     }
+    NSData* data = [formatedParameters dataUsingEncoding:NSUTF8StringEncoding];
     NSURL* url = [NSURL URLWithString:[ServerUrl stringByAppendingString:postposition]];
     NSLog(@"URL: %@", url);
     NSLog(@"Parameters: %@", formatedParameters);
     NSMutableURLRequest* request = [NSMutableURLRequest requestWithURL:url cachePolicy:NSURLRequestUseProtocolCachePolicy timeoutInterval:6];
     [request setHTTPMethod:@"POST"];
+    [request addValue:@"application/x-www-form-urlencoded" forHTTPHeaderField:@"Content-Type"];
+    [request addValue:[NSString stringWithFormat:@"%lu", (unsigned long)[formatedParameters length]] forHTTPHeaderField:@"Content-Length"];
+    [request setHTTPBody:data];
     NSURLResponse* response = nil;
-    NSData *data = [NSURLConnection sendSynchronousRequest:request returningResponse:&response error:&error];
-    if([data length] > 0 && error == nil) {
-        NSMutableDictionary* jsonObject = [NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingAllowFragments error:&error];
+    NSData* body = [NSURLConnection sendSynchronousRequest:request returningResponse:&response error:&error];
+    if([body length] > 0 && error == nil) {
+        NSMutableDictionary* jsonObject = [NSJSONSerialization JSONObjectWithData:body options:NSJSONReadingAllowFragments error:&error];
         if(error == nil) {
             return jsonObject;
         }
