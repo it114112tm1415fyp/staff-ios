d1de4ce454e8a1cb2fe07f7f8ea5633f04f8dc95
lokifung <zacklo1994@yahoo.com.hk>
2014-12-10 13:35:05 +0800

    Initial Commit

---
+++ Staff-IOS/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Staff-IOS
+//
+//  Created by Patrick Lo on 10/12/14.
+//  Copyright (c) 2014 1415FYP. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Staff-IOS/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Staff-IOS
+//
+//  Created by Patrick Lo on 10/12/14.
+//  Copyright (c) 2014 1415FYP. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
